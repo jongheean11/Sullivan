35f0d4e302968d19c8ff87dd1e160307e6ae7729
James Kim <jongheean11@nate.com>
2015-11-17 14:45:24 +0900

    Merging

--- App2/MainPage.xaml.cs
+++ App2/MainPage.xaml.cs
@@ -12,7 +12,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using FTD2XX_NET;
 using Windows.UI;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
